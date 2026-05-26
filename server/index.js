a5452df49d3c342c8e76c1734d8988fb50e99536
AdItYaSiNhG <adityasingh10072003@gmail.com>
2026-05-27 04:03:19 +0530

    fix for the deploy error

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { runPipeline } from '../core/pipeline.js';
 import { getAllMetrics } from '../core/stage7_evaluate.js';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+// ESM-compatible __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 
